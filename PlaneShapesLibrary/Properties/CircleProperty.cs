828201868607ea31df4552ec5367c0f69c4735aa
Nodira Kosimova <nkosimova06@gmail.com>
2022-09-14 12:59:04 +0300

    write console application code for client

--- PlaneShapesLibrary/Properties/CircleProperty.cs
+++ PlaneShapesLibrary/Properties/CircleProperty.cs
@@ -1,7 +1,7 @@
 public class CircleProperty
 {
     public double Radius { get; set; }  
-    public CircleProperty(int radius)
+    public CircleProperty(double radius)
     {
         if (radius <= 0)
             throw new InvalidDataException("PropertyError: Radius must be positive number");
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,9 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using PlaneShapesLibrary;
 
-var factory = new PlaneShapeFactory(); 
-var properties = new CircleProperty(10);
+IShape shape;
+var factory = new PlaneShapeFactory();
+while (true)
+{
+    Console.WriteLine("Choose action:");
+    Console.WriteLine("1 - Find the Area and the Perimeter for triangle");
+    Console.WriteLine("2 - Find the Area and the Perimeter for circle");
+    Console.WriteLine("0 - Exit");
 
-var newShape = factory.CreateShape(properties);
+    var action = int.Parse(Console.ReadLine());
+    if (action == 0) break;
 
-Console.WriteLine(newShape.GetTheArea());
+    switch(action)
+    {
+        case 0:
+            break;
+        case 1:
+            Console.WriteLine("Enter the value of the sides  a, b, c separated by a space:");
+            var data = Console.ReadLine().Split(' ').Select(x => double.Parse(x)).ToArray();
+            try
+            {
+                var property = new TriangleProperty(data[0], data[1], data[2]);
+                shape = factory.CreateShape(property);
+                Console.WriteLine("P = " + shape.GetThePerimeter().ToString());
+                Console.WriteLine("S = " + shape.GetTheArea().ToString());
+
+                Console.WriteLine("-----------------");
+                Console.WriteLine();
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            break;
+        case 2:
+            Console.WriteLine("Enter the value of the radius:");
+            var r = double.Parse(Console.ReadLine());
+            try
+            {
+                var property = new CircleProperty(r);
+                shape = factory.CreateShape(property);
+                Console.WriteLine("P = " + shape.GetThePerimeter().ToString());
+                Console.WriteLine("S = " + shape.GetTheArea().ToString());
+
+                Console.WriteLine("-----------------");
+                Console.WriteLine();
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            break;
+    }
+
+}
